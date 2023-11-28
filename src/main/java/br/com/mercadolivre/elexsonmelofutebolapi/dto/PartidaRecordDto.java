bd846453ec7bfc088764544bac3280ee58bbe9e1
xmelo_meli <elexson.melo@mercadolivre.com>
2023-11-28 14:29:55 -0300

    Implementacao de testes unitários

--- src/main/java/br/com/mercadolivre/elexsonmelofutebolapi/dto/PartidaRecordDto.java
+++
@@ -1,7 +0,0 @@
-package br.com.mercadolivre.elexsonmelofutebolapi.dto;
-
-import java.time.LocalDateTime;
-
-public record PartidaRecordDto(String clubeMandante, String clubeVisitante, int resultado,int resultadoMandante,int resultadoVisitante, LocalDateTime dataHora) {
-
-}
